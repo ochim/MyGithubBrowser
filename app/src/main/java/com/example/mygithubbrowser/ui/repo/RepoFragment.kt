a319ed0eeebf05dea32910b335f91a7a6c5a7e23
ochi <mune.ochi@gmail.com>
2020-09-20 01:12:39 +0900

    setup Navigation Componnent, SearchFragment, SearchViewModel,RepoFragment, RepoViewModel, etc.

---
+++ app/src/main/java/com/example/mygithubbrowser/ui/repo/RepoFragment.kt
@@ -0,0 +1,32 @@
+package com.example.mygithubbrowser.ui.repo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.mygithubbrowser.R
+
+class RepoFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RepoFragment()
+    }
+
+    private lateinit var viewModel: RepoViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.repo_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(RepoViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/mygithubbrowser/ui/repo/RepoViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mygithubbrowser.ui.repo
+
+import androidx.lifecycle.ViewModel
+
+class RepoViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
